3f93f28eef1996ac21737dc3db0cc51c758aa7cb
MaelDV <skaan.dmv@gmail.com>
2026-02-03 21:21:25 -0500

    Use mc.png from public directory instead of S3 for email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://mastercolorfiles.s3.us-east-2.amazonaws.com/emails/mc-logo.png" alt="Master Color" style="height: 50px; width: 50px; max-width: 50px; display: block;">
+<img src="{{ config('app.url') }}/mc.png" alt="Master Color" style="height: 50px; width: 50px; max-width: 50px; display: block;">
 @else
 {!! $slot !!}
 @endif
