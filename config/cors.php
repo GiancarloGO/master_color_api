150ef9c17cff48940c0ba9905a0de91aa6618c6b
GiancarloGo <ggarciaol92@gmail.com>
2026-08-30 14:10:43 -0500

    chore: actualizar politicas de CORS para cloudflare y ajustar config composer

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,14 @@
 <?php
-
 return [
     'paths' => ['api/*'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', ''))),
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins_patterns' => [
+        '#^https://.*\.pages\.dev$#',
+        '#^https://.*\.workers\.dev$#'
+    ],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 ];
